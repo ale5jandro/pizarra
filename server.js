cd956dd28bd957134275ba77c1cf437e0e15e8c0
Ale <alejandro.biagetti@ross.com.ar>
2022-09-10 21:59:45 -0300

    detetct metamask

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.static(__dirname + '/static'));
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
